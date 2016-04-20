97f5e7e897212c7b27f8c4c39f9bf1645c28acd5
bluerainla <bluerainla@gmail.com>
2016-04-20 14:58:32 +0800

    frm分開

---
+++ coperation/frm修改資料.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coperation
+{
+    public partial class frm修改資料 : Form
+    {
+        public frm修改資料()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ coperation/frm搜尋寵物住宿.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace coperation
+{
+    partial class frm搜尋寵物住宿
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSearchMe = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmbSearchMyPet = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbSearchMyPetRequireArea = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearchALL = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbSearchAllPetRequireArea = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbSearchPetType = new System.Windows.Forms.ComboBox();
+            this.cmbSearchPetSize = new System.Windows.Forms.ComboBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(553, 150);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 12);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "我的寵物住宿";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSearchMe);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.cmbSearchMyPet);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.cmbSearchMyPetRequireArea);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(458, 174);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(318, 303);
+            this.panel2.TabIndex = 17;
+            // 
+            // btnSearchMe
+            // 
+            this.btnSearchMe.Location = new System.Drawing.Point(122, 222);
+            this.btnSearchMe.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearchMe.Name = "btnSearchMe";
+            this.btnSearchMe.Size = new System.Drawing.Size(80, 34);
+            this.btnSearchMe.TabIndex = 10;
+            this.btnSearchMe.Text = "搜尋";
+            this.btnSearchMe.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "我的寵物";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(154, 110);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 22);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // cmbSearchMyPet
+            // 
+            this.cmbSearchMyPet.FormattingEnabled = true;
+            this.cmbSearchMyPet.Location = new System.Drawing.Point(154, 18);
+            this.cmbSearchMyPet.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbSearchMyPet.Name = "cmbSearchMyPet";
+            this.cmbSearchMyPet.Size = new System.Drawing.Size(92, 20);
+            this.cmbSearchMyPet.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "需求區域";
+            // 
+            // cmbSearchMyPetRequireArea
+            // 
+            this.cmbSearchMyPetRequireArea.FormattingEnabled = true;
+            this.cmbSearchMyPetRequireArea.Location = new System.Drawing.Point(154, 64);
+            this.cmbSearchMyPetRequireArea.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbSearchMyPetRequireArea.Name = "cmbSearchMyPetRequireArea";
+            this.cmbSearchMyPetRequireArea.Size = new System.Drawing.Size(92, 20);
+            this.cmbSearchMyPetRequireArea.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 110);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "需求日期";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(195, 150);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "所有寵物住宿";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSearchALL);
+            this.panel1.Controls.Add(this.dateTimePicker2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.cmbSearchAllPetRequireArea);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cmbSearchPetType);
+            this.panel1.Controls.Add(this.cmbSearchPetSize);
+            this.panel1.Location = new System.Drawing.Point(108, 174);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 303);
+            this.panel1.TabIndex = 16;
+            // 
+            // btnSearchALL
+            // 
+            this.btnSearchALL.Location = new System.Drawing.Point(104, 222);
+            this.btnSearchALL.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearchALL.Name = "btnSearchALL";
+            this.btnSearchALL.Size = new System.Drawing.Size(80, 34);
+            this.btnSearchALL.TabIndex = 15;
+            this.btnSearchALL.Text = "搜尋";
+            this.btnSearchALL.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(128, 162);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(151, 22);
+            this.dateTimePicker2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-4, 119);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "需求區域";
+            // 
+            // cmbSearchAllPetRequireArea
+            // 
+            this.cmbSearchAllPetRequireArea.FormattingEnabled = true;
+            this.cmbSearchAllPetRequireArea.Location = new System.Drawing.Point(128, 117);
+            this.cmbSearchAllPetRequireArea.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbSearchAllPetRequireArea.Name = "cmbSearchAllPetRequireArea";
+            this.cmbSearchAllPetRequireArea.Size = new System.Drawing.Size(92, 20);
+            this.cmbSearchAllPetRequireArea.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(-4, 162);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "需求日期";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-4, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "寵物類型";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-4, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "寵物體型";
+            // 
+            // cmbSearchPetType
+            // 
+            this.cmbSearchPetType.FormattingEnabled = true;
+            this.cmbSearchPetType.Location = new System.Drawing.Point(128, 37);
+            this.cmbSearchPetType.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbSearchPetType.Name = "cmbSearchPetType";
+            this.cmbSearchPetType.Size = new System.Drawing.Size(92, 20);
+            this.cmbSearchPetType.TabIndex = 4;
+            // 
+            // cmbSearchPetSize
+            // 
+            this.cmbSearchPetSize.FormattingEnabled = true;
+            this.cmbSearchPetSize.Location = new System.Drawing.Point(128, 75);
+            this.cmbSearchPetSize.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbSearchPetSize.Name = "cmbSearchPetSize";
+            this.cmbSearchPetSize.Size = new System.Drawing.Size(92, 20);
+            this.cmbSearchPetSize.TabIndex = 5;
+            // 
+            // frm搜尋寵物住宿
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 626);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm搜尋寵物住宿";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSearchMe;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmbSearchMyPet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbSearchMyPetRequireArea;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSearchALL;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbSearchAllPetRequireArea;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbSearchPetType;
+        private System.Windows.Forms.ComboBox cmbSearchPetSize;
+    }
+}
+
